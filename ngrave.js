be3258f126df196492724cb6788a6779fec44a1a
johnlenon <johnlenonmaghanoy@gmail.com>
2017-06-19 15:40:04 +0800

    Updated client test.

--- ngrave.js
+++ ngrave.js
@@ -35,6 +35,10 @@
 			"module": "ngrave",
 			"author": "Richeve S. Bebedor",
 			"eMail": "richeve.bebedor@gmail.com",
+			"contributors": [
+				"John Lenon Maghanoy <johnlenonmaghanoy@gmail.com>",
+				"Vinse Vinalon <vinsevinalon@gmail.com>"
+			],
 			"repository": "https://github.com/volkovasystems/ngrave.git",
 			"test": "ngrave-test.js",
 			"global": true
